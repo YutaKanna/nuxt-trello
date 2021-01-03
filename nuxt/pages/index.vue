499b4e84bb21dac6acd4442f9620e4a986b1281f
YutaKanna <kannayuta0629@gmail.com>
2021-01-03 14:21:14 +0900

    一次元で縦の移動をできるようにする

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,73 +1,28 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div id="app">
+    <draggable element="ul">
+      <li v-for="task in tasks">{{ task }}</li>
+    </draggable>
   </div>
 </template>
 
 <script>
-export default {}
+import draggable from 'vuedraggable';
+
+export default {
+  name: 'app',
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      tasks: [
+        'task1',
+        'task2',
+        'task3',
+        'task4',
+      ]
+    }
+  }
+}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
