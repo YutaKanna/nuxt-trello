88237e9e10e6f74b540247b19e00d2c854a0bd0f
YutaKanna <kannayuta0629@gmail.com>
2021-01-03 14:38:02 +0900

    二つのエリアをドラッグ移動可能にする

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="app">
-    <draggable tag="ul">
+    <div id="box1" class="box">
+      <draggable tag="ul" :options="{group:'ITEMS'}">
         <li v-for="item, index in items" :key="item.no">{{item.name}}-(No.{{item.no}})</li>
-    </draggable>
+      </draggable>
+    </div>
+    <div id="box2" class="box">
+      <draggable tag="ul" :options="{group:'ITEMS'}">
+        <li v-for="item, index in items2" :key="item.no">{{item.name}}-(No.{{item.no}})</li>
+      </draggable>
+    </div>
   </div>
 </template>
 
@@ -12,27 +19,44 @@ import draggable from 'vuedraggable';
 export default {
   name: 'app',
   components: {
-    draggable,
+    draggable
   },
-  data() {
+  data: function () {
     return {
-        items:[
-          {no:1, name:'キャベツ', categoryNo:'1'},
-          {no:2, name:'ステーキ', categoryNo:'2'},
-          {no:3, name:'リンゴ', categoryNo:'3'}
-        ]
+      items:[
+        {no:1, name:'キャベツ', categoryNo:'1'},
+        {no:2, name:'ステーキ', categoryNo:'2'}
+      ],
+      items2:[
+        {no:5, name:'きゅうり', categoryNo:'1'},
+        {no:6, name:'ハンバーグ', categoryNo:'2'}
+      ]
     }
   }
 }
 </script>
 
 <style>
-ul {
-  list-style-type: none;
+#app {
+  padding : 15px;
 }
+
+.box {
+  width: 50%;
+  float: left;
+  padding: 20px 0;
+}
+#box1 {
+  background-color: #fdd;
+}
+#box2 {
+  background-color: #ddf;
+}
+
 li {
-  cursor: pointer;
+  cursor:pointer;
   padding: 10px;
   border: solid #ddd 1px;
+  background-color: #fff;
 }
 </style>
