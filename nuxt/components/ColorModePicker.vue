99250ca585618bc9b0531d4e40dd675c24a62418
Yuta Kanna <yutakanna@YutanoMacBook-Air.local>
2021-11-11 08:43:11 +0900

    ダークモード実装

---
+++ nuxt/components/ColorModePicker.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <li
+      v-for="color of colors"
+      :key="color"
+      @click="$colorMode.preference = color"
+    >{{color}}</li>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        colors: ['system', 'light', 'dark', 'sepia']
+      }
+    }
+  }
+</script>
